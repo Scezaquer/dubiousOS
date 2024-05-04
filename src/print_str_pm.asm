adc09e3f8ce22e4152cf1ee8c7e208983179e40f
Scezaquer <aurelien.buck.kaeffer@gmail.com>
2024-05-04 02:02:36 -0400

    32bits print

---
+++ src/print_str_pm.asm
@@ -0,0 +1,27 @@
+;32 bits version of the print_string routine (protected mode)
+bits 32
+
+; Constants
+VIDEO_MEMORY eqn 0xb8000
+WHITE_ON_BLACK eqn 0x0f
+
+;This takes EBX as argument (the address of a null terminated string)
+print_string_pm:
+    pusha
+    mov edx, VIDEO_MEMORY   ; pointer to the video memory
+
+print_string_pm_loop:
+    mov al, [ebx]           ; load character
+
+    cmp al, 0x00            ; Check we haven't reached the end of the string
+    je end_print_string     ; break if we did
+
+    mov ah, WHITE_ON_BLACK  ; load attributes
+    mov [edx], ax           ; print char 
+    add edx, 2              ; move the pointer to next char cell in video memory
+    add ebx, 1              ; move to next character in string
+    jmp loop_print_string
+
+end_print_string:
+    popa
+    ret
