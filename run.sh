000c5c5c598f7a7c31cf4a3ef049101bf77e4efe
Scezaquer <aurelien.buck.kaeffer@gmail.com>
2024-05-04 20:24:49 -0400

    rust kernel initial commit

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-while getopts a flag
+# Handle the -a and -c flags
+while getopts ac flag
 do
     case "${flag}" in
-        a) ./assemble.sh
+        # -a flag assembles the boot sector
+        a) ./assemble.sh ;;
+        # -c flag compiles the kernel
+        c) 
+            rustc --target x86_64-unknown-none -O --emit obj src/kernel.rs -o build/kernel.o
+            ld -o build/kernel.bin -Ttext 0x1000 build/kernel.o --oformat binary;;
     esac
 done
 
+# run the cpu emulator with the kernel image we made
 qemu-system-x86_64 -drive format=raw,file=build/boot_sector.bin
---
+++ src/kernel.rs
@@ -0,0 +1,35 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // Initialization code goes here
+    // This is where you would set up your environment, initialize hardware, etc.
+
+    // Call your previous main function
+    main();
+
+    // This function should not return, so we use an infinite loop
+    loop {}
+}
+
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // add custom panic handling logic here
+
+    // Halt the CPU or loop indefinitely to prevent further execution
+    loop {}
+}
+
+fn main(){
+    // Define a mutable raw pointer to video memory location 0xb8000 (top left
+    // corner of the screen)
+    let video_mem_ptr: *mut u8 = 0xB8000 as *mut u8;
+
+    // Set the character pointed to. Unsafe is necessary when dereferencing a
+    // raw pointer
+    unsafe{ *video_mem_ptr = b'X'; }
+}
